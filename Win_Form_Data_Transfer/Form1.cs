da897453f92ccb140b5af2760e1ed1656482b3c0
alesg <alesg@DESKTOP-HOUSE>
2017-03-21 08:27:00 +0100

    Add project files.

---
+++ Win_Form_Data_Transfer/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Win_Form_Data_Transfer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ClickOpenForm2(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
